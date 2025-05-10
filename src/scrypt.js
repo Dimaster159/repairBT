d7810e532c5689fa75728fdbe8f1c1e71ef5a3d7
Dmitriy Pastuhov <remsma702@yandex.ru>
2025-05-10 15:07:01 +0500

    1.1.6

--- src/scrypt.js
+++ src/scrypt.js
@@ -1,5 +1,10 @@
-function integrate(coefficient, exponent) {
-    let newCoefficient = coefficient / (exponent + 1);
-    let newExponent = exponent ++;
-    return `${newCoefficient}x^${newExponent}`;
+function quote(fighter) {
+  // your code here
+  if(fighter.toLowerCase() == 'george saint pierre'){
+    return "I am not impressed by your performance.";
+  } else {
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
   }
+};
+
+console.log(quote('George Saint Pierre'))
